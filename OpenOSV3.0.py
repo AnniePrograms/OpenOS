b32b81355e43ef44ab07fe6229ddc5c865b69e49
Annie <169003528+AnniePrograms@users.noreply.github.com>
2025-04-15 15:58:28 -0400

    Update and rename OpenOSV2.2.py to OpenOSV3.0.py

--- OpenOSV3.0.py
+++ OpenOSV3.0.py
@@ -17,9 +17,10 @@ print('''\033[36;1m    ███████                                    
    ░░░░░░░     ░███░░░   ░░░░░░  ░░░░ ░░░░░    ░░░░░░░     ░░░░░░░░░  
                ░███                                                   
                █████                                                  
-              ░░░░░                                                   \033[0m''')
+              ░░░░░                                                   
+Developed by Annie\033[0m''')
 try:
-	print('\nChecking version status via cloud...')
+	print('\nChecking version status...')
 	CloudRequestVersion = get('https://gist.githubusercontent.com/AnniePrograms/29c5e07f6be1694c70d5372627f77a01/raw/Version.txt')
 	CloudRequestVersion.raise_for_status()
 	if CloudRequestVersion.text.splitlines()[1] == 'n':
@@ -32,7 +33,7 @@ except Exception as ErrorDetails1:
 	exit()
 print('Success')
 try:
-	print('Loading apps via cloud...')
+	print('Installing apps...')
 	CloudRequestAppsList = get('https://gist.githubusercontent.com/AnniePrograms/29c5e07f6be1694c70d5372627f77a01/raw/AppsList.txt')
 	CloudRequestAppsList.raise_for_status()
 	for AppItem in CloudRequestAppsList.text.splitlines():
@@ -40,6 +41,11 @@ try:
 		CloudRequestAppItem.raise_for_status()
 		AppsDictionary[CloudRequestAppItem.text.splitlines()[0]] = '--nl'.join(CloudRequestAppItem.text.splitlines()[1:])
 		AppsNameList.append(CloudRequestAppItem.text.splitlines()[0])
+	localapps = open('localappdirlist.txt', 'r').readlines()
+	for app in localapps:
+		indapp = open(app,'r').readlines()
+		AppsDictionary[indapp[0][0:]] = indapp[1:]
+		AppsNameList.append(indapp[0][0:])
 except Exception as ErrorDetails2:
 	print(f'\033[31;1mSomething went wrong loading the apps!\n\nError Details:\n{ErrorDetails2}\033[0m')
 	input('\033[36;1mLoad Failed, submit an issue at https://github.com/AnniePrograms/OpenOS/ -\033[0m ')
